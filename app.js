db62682798d12f98f7473146070b19526361c209
tarwat-uddin <t.uddin9121@gmail.com>
2021-12-20 16:42:15 +0000

    Update app.js
    Major changes

--- app.js
+++ app.js
@@ -5,49 +5,49 @@
 ///////////////////////////////////////////////
 
 // Parents
-const containerMain = document.querySelector(".main");
-const containerHero = document.querySelector(".main__head");
-const containerLoad = document.querySelector(".load");
+const containerHead = document.querySelector(".head");
+const containerMain = document.querySelector(".main__content");
+const containerHero = document.querySelector(".content__container--hero");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
 // Buttons
-const btnUpdateWeather = document.querySelector(".head .nav__btn");
+const btnUpdate = containerHead.querySelector(".nav__btn--update");
+const btnClose = modal.querySelector(".modal__btn--close");
 // Inputs
-const progressBar = document.querySelector(".load__bar");
+const inputKey = modal.querySelector(".modal__input--key");
 // Labels
-const labelLoadDesc = document.querySelector(".load__label--desc");
-const labelWeatherTitle = document.querySelector(".hero__label--title");
-const labelWeatherDesc = document.querySelector(".hero__label--desc");
-const labelWeatherTemp = document.querySelector(".hero__label--temp");
-const labelWeatherHumidity = document.querySelector(".hero__label--humidity");
+const labelTitle = containerHero.querySelector(".container__label--title");
+const labelDesc = containerHero.querySelector(".item__label--desc");
+const labelTemp = containerHero.querySelector(".item__label--temp");
+const labelHumidity = containerHero.querySelector(".item__label--humidity");
 // Images
-const imgWeatherIcon = document.querySelector(".main .hero__img");
+const imgIcon = containerHero.querySelector(".item__img");
 
 ////////////////////////////////////////////////
 ////// App Architecture
 ///////////////////////////////////////////////
 
 class App {
+  #key;
+
   constructor() {
-    btnUpdateWeather.addEventListener(
-      "click",
-      this._retrieveWeatherData.bind(this)
-    );
+    // Add event handlers
+    btnUpdate.addEventListener("click", this._retrieveWeatherData.bind(this));
+    btnClose.addEventListener("click", this._receiveKey.bind(this));
   }
 
-  _updateProgressBar(val) {
-    let txt;
-    progressBar.value = val;
-    switch (val) {
-      case 20:
-        txt = "Fetching user location";
-        break;
-      case 50:
-        txt = "Gathering locale weather updates";
-        break;
-      case 99:
-        txt = "Rendering weather data";
-        break;
+  _receiveKey() {
+    this.#key = inputKey?.value;
+    if (!this.#key)
+      return alert(
+        "You must enter an API key in order to use this app. Please go to the README file of this repo to find out how"
+      );
+    if (!(this.#key.length === 32)) {
+      inputKey.value = "";
+      return alert("This key might be invalid. Please try again :)");
     }
-    labelLoadDesc.textContent = `${txt}...`;
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
   }
 
   _getPosition() {
@@ -58,61 +58,42 @@ class App {
 
   async _retrieveWeatherData() {
     try {
-      const wait = (secs) =>
-        new Promise((resolve) => setTimeout(resolve, secs * 1000));
+      if (!this.#key) return;
       containerHero.classList.add("hidden");
-      containerLoad.classList.remove("hidden");
       const pos = await this._getPosition();
       const { latitude: lat, longitude: lon } = pos.coords;
-      this._updateProgressBar(20);
-      await wait(1);
       const geoRes = await fetch(
         `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}&localityLanguage=en`
       );
       if (!geoRes.ok) throw new Error("Problem with geocoding");
-      this._updateProgressBar(50);
-      await wait(1);
       const geoData = await geoRes.json();
       const weatherRes = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${geoData.city}&units=metric&appid={YOURKEY}}`
+        `https://api.openweathermap.org/data/2.5/weather?q=${
+          geoData.city
+        }&units=metric&appid=${this.#key}`
       );
       if (!weatherRes.ok) throw new Error("Failed to retrieve weather data");
-      this._updateProgressBar(99);
-      await wait(1);
-      containerLoad.classList.add("hidden");
       const weatherData = await weatherRes.json();
       this._renderWeather(weatherData);
     } catch (err) {
-      console.error(err);
-      throw err;
+      console.error(err.message);
     }
   }
 
   _renderWeather(data) {
-    let imgName;
     const { temp, humidity } = data.main;
     const [weather] = data.weather;
     const { main: title, description: desc } = weather;
-    switch (title.toLowerCase()) {
-      case "clouds":
-        imgName = "clouds";
-        break;
-      case "rain":
-        imgName = "rain";
-        break;
-      case "sunny":
-        imgName = "sunny";
-        break;
-    }
+    const imgName = title.toLowerCase();
     containerMain.style.backgroundImage = `url(/assets/images/${imgName}.jpg)`;
     const imgPath = `https://openweathermap.org/img/wn/${weather.icon}@2x.png`;
-    imgWeatherIcon.src = imgPath;
-    imgWeatherIcon.addEventListener("load", function () {
+    imgIcon.src = imgPath;
+    imgIcon.addEventListener("load", function () {
       containerHero.classList.remove("hidden");
-      labelWeatherTitle.textContent = title;
-      labelWeatherTemp.textContent = `${temp}°`;
-      labelWeatherHumidity.textContent = `${humidity}%`;
-      labelWeatherDesc.textContent = desc;
+      labelTitle.textContent = title;
+      labelTemp.textContent = `${temp}°`;
+      labelHumidity.textContent = `${humidity}%`;
+      labelDesc.textContent = desc;
     });
   }
 }
